95a56aad9d63b92c032048c8dfb92fb8b7d0ae22
WML2013 <mingles@gmail.com>
2018-06-18 11:44:47 +0100

    update

---
+++ createFolder.ps1
@@ -0,0 +1,16 @@
+﻿    IF (!(TEST-PATH C:\agsusage)) {
+                    NEW-ITEM C:\agsusage -type Directory
+    }
+    IF (!(TEST-PATH C:\agswork)) {
+                    NEW-ITEM C:\agswork -type Directory
+    }
+
+    IF (!(TEST-PATH C:\lhlog)) {
+                    NEW-ITEM C:\lhlog -type Directory
+    }
+
+    IF (!(TEST-PATH C:\Logs)) {
+
+    NEW-ITEM C:\Logs -type Directory
+
+        }
